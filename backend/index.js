9d51999079ed9d7b43b8c51750d2b9bedad6872f
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2024-12-10 10:43:25 +0530

    initial commit of backend

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+require('dotenv').config();
+
+const PORT=process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`App is listening to port ${PORT}`);
+})
+
+app.get('/',(req,res)=>{
+    res.send(`<h1>Hello World</h1>`);
+})
