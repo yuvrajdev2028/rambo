3135205049c0397dd482a313bfe150a13630848a
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-02-01 04:36:36 +0530

    added refresh route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {signup,login,logout} = require('../controllers/userControllers')
+const {signup,login,logout,refresh} = require('../controllers/userControllers')
 
 const router = express.Router();
 
 router.post('/signup',signup);
 router.post('/login',login);
 router.post('/logout',logout);
+router.post('/refresh',refresh)
 
 module.exports = router;
