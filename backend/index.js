1f76e5d08f68b4a231b7963a79485d10ef280ca9
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-02-01 04:33:28 +0530

    setup helmet and cors for security and access control

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,28 @@
 const express=require('express');
-const dbConnect= require('./config/database')
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
+const helmet=require('helmet');
+const cors=require('cors');
+const dbConnect= require('./config/database')
 const user = require('./routes/userRoutes');
 const response = require('./routes/responseRoutes');
 const report = require('./routes/reportRoutes');
-const cookieParser = require('cookie-parser');
 
 const PORT=process.env.PORT || 5000;
 
 const app=express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');  // Allow only this origin
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');  // Allow only this origin
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     next();
+// });
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(helmet());
+app.use(cors({origin: 'http://localhost:3000', credentials:true}))
 
 app.use('/api/v1',user);
 app.use('/api/v1',response);
