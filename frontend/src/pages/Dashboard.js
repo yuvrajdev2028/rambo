157edf5dcd3366f4f31a4a5ead4e1933e872211f
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-02-03 23:57:15 +0530

    added role-specific dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import NGODashboard from '../components/NGODashboard'
+import VolunteerDashboard from '../components/VolunteerDashboard'
+import AuthContext from '../contexts/AuthContext'
 
 const Dashboard = () => {
+  const { user } = useContext(AuthContext)
+  const role = user.role;
   return (
-    <div>Dashboard</div>
+    <div>
+      {role==="ngo"?(<NGODashboard/>):(<VolunteerDashboard/>)}
+    </div>
   )
 }
 
