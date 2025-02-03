21d25ac6bca1efdd50f61b7e05ed24ba9b0a3548
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-02-03 23:56:00 +0530

    fixed Login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import LoginForm from '../components/LoginForm'
 
-const Login = ({setIsLoggedIn}) => {
+const Login = () => {
   return (
     <div className='login-container h-[70%] w-[30%] drop-shadow-md bg-white my-8 mx-auto p-4'>
       <h3 className='font-thin text-2xl mb-10 text-green-600'>LogIn</h3>
-      <LoginForm setIsLoggedIn={setIsLoggedIn}/>
+      <LoginForm/>
     </div>
   )
 }
