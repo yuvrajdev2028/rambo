473ca16020970695178017e6b738344847a9b52f
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-02-03 23:50:04 +0530

    integrated usage of AuthContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
@@ -8,26 +7,25 @@ import About from './pages/About';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
-// import RecentPosts from './pages/RecentPosts';
 import Footer from './components/Footer';
 import NotFound from './pages/NotFound';
-import { useState } from 'react';
+import { useContext } from 'react';
+import AuthContext from './contexts/AuthContext';
 
 
 function App() {
-  const [isLoggedin,setIsLoggedIn]=useState(false);
+  const { user } = useContext(AuthContext);
 
   return (
     <div className="App">
-      <Navbar isLoggedin={isLoggedin}/>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/community' element={<Community/>} />
-        <Route path='/dashboard' element={isLoggedin?(<Dashboard/>):(<NotFound/>)} />
-        <Route path='/login' element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
-        <Route path='/signup' element={<Signup setIsLoggedIn={setIsLoggedIn}/>} />
-        {/* <Route path='/recentposts' element={<RecentPosts/>} /> */}
+        <Route path='/dashboard' element={user?(<Dashboard/>):(<NotFound/>)}/>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
         <Route path='*' element={<NotFound/>} />
       </Routes>
       <Footer/>
