a57579f7e2c1a206eb777f681003e38346363e48
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-03-17 18:40:24 +0530

    fixed height of signup form

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -3,7 +3,7 @@ import SignupForm from '../components/SignupForm'
 
 const Signup = () => {
   return (
-    <div className='login-container h-[70%] w-[30%] drop-shadow-md bg-white my-8 mx-auto p-4'>
+    <div className='login-container w-[30%] drop-shadow-md bg-white my-8 mx-auto p-4'>
       <h3 className='font-thin text-2xl mb-10 text-green-600'>SignUp</h3>
       <SignupForm/>
     </div>
