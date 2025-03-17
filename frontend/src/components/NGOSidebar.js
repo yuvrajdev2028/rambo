c26eaf6748fd88220c79c039a99b90e849301c63
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-03-17 18:49:06 +0530

    fixed NGO sidebar

--- frontend/src/components/NGOSidebar.js
+++ frontend/src/components/NGOSidebar.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import SidebarTab from './SidebarTab'
 
-const NGOSidebar = () => {
+const NGOSidebar = ({tab,setTab}) => {
   return (
-    <div>
-        <SidebarTab label={"Dashboard"}/>
-        <SidebarTab label={"Incoming Reports"}/>
-        <SidebarTab label={"Active Cases"}/>
-        <SidebarTab label={"Resolved Cases"}/>
-        <SidebarTab label={"Profile"}/>
+    <div className='border-r-4 border-r-green-600 min-w-fit px-4'>
+        <SidebarTab label={'Profile'} tab={tab} setTab={setTab}/>
+        <SidebarTab label={'Incoming Reports'} tab={tab} setTab={setTab}/>
+        <SidebarTab label={'Active Cases'} tab={tab} setTab={setTab}/>
+        <SidebarTab label={'Resolved Cases'} tab={tab} setTab={setTab}/>
     </div>
   )
 }
