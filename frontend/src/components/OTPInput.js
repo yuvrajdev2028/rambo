d887276d4e7eac8e4c38f8ac7d064c9e55a4fb10
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-04-15 14:04:46 +0530

    added OTP input

---
+++ frontend/src/components/OTPInput.js
@@ -0,0 +1,70 @@
+import {useEffect, useRef, useState} from "react";
+
+const OTPInput = ({length = 4, onOTPSubmit = () => {}}) => {
+  const [otp, setOtp] = useState(new Array(length).fill(""));
+  const inputRefs = useRef([]);
+
+  useEffect(() => {
+    if (inputRefs.current[0]) {
+      inputRefs.current[0].focus();
+    }
+  }, []);
+
+  const handleChange = (index, e) => {
+    const value = e.target.value;
+    if (isNaN(value)) return;
+
+    const newOtp = [...otp];
+
+    newOtp[index] = value.substring(value.length - 1);
+    setOtp(newOtp);
+
+
+    const combinedOtp = newOtp.join("");
+    if (combinedOtp.length === length) onOTPSubmit(combinedOtp);
+
+    if (value && index < length - 1 && inputRefs.current[index + 1]) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleClick = (index) => {
+    inputRefs.current[index].setSelectionRange(1, 1);
+
+    if (index > 0 && !otp[index - 1]) {
+      inputRefs.current[otp.indexOf("")].focus();
+    }
+  };
+
+  const handleKeyDown = (index, e) => {
+    if (
+      e.key === "Backspace" &&
+      !otp[index] &&
+      index > 0 &&
+      inputRefs.current[index - 1]
+    ) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  return (
+    <div className="flex gap-2 justify-center">
+      {otp.map((value, index) => {
+        return (
+          <input
+            key={index}
+            type="text"
+            ref={(input) => (inputRefs.current[index] = input)}
+            value={value}
+            onChange={(e) => handleChange(index, e)}
+            onClick={() => handleClick(index)}
+            onKeyDown={(e) => handleKeyDown(index, e)}
+            className="otpInput h-10 w-10 bg-[#EFF0F2] text-center"
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default OTPInput;
