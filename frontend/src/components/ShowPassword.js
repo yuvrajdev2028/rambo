e84288a502e58453dc567adea1e606d54c2fe013
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-04-15 14:05:53 +0530

    added font awesome icons for eye and eyeslash in show password component

---
+++ frontend/src/components/ShowPassword.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+const ShowPassword = ({hidePassword,setHidePassword}) => {
+
+    const clickHandler = (event)=>{
+        event.preventDefault();
+        if(hidePassword) setHidePassword(false);
+        else setHidePassword(true);
+    }
+  return (
+    <button onClick={clickHandler} className='border border-gray-400 p-2 w-10'>
+        {(hidePassword)?(<FontAwesomeIcon icon={faEyeSlash} />):(<FontAwesomeIcon icon={faEye} />)}
+    </button>
+  )
+}
+
+export default ShowPassword
