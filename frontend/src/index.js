d28af77a0a5dde9122615befe6cc0603ce9359aa
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-04-15 14:01:14 +0530

    wrapped App component in UtilsProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import { UtilsProvider } from './contexts/UtilsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <UtilsProvider>
+        <App />
+      </UtilsProvider>
     </AuthProvider>
   </BrowserRouter>
     
