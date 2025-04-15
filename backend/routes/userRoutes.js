d22f9eac689c7294e45576db2631a1c462b7edc7
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-04-15 13:52:54 +0530

    added validateUser, sendOTP, verifyOTP, changePassword routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const {signup,login,logout,refresh} = require('../controllers/userControllers')
+const {signup,login,logout,refresh, validateUser, sendOTP, verifyOTP, changePassword} = require('../controllers/userControllers');
 
 const router = express.Router();
 
 router.post('/signup',signup);
 router.post('/login',login);
 router.post('/logout',logout);
-router.post('/refresh',refresh)
+router.post('/refresh',refresh);
+router.post('/validateUser',validateUser);
+router.post('/sendOTP',sendOTP);
+router.post('/verifyOTP',verifyOTP);
+router.post('/changePassword',changePassword);
 
 module.exports = router;
