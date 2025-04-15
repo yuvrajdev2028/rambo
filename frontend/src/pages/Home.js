87e45c7f17b4dcc5b1dbc3994c8d61d26fa059c2
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-04-15 14:09:49 +0530

    added link to signup and about pages

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -9,8 +9,8 @@ const Home = () => {
         <h3 className='text-5xl font-bold text-green-600'>Connecting compassion.</h3>
         <p className='font-thin text-3xl '>Rambo is a platform devoted to connecting compassionate volunteers to NGOs working to save lives of humankind's best freinds.</p>
         <div className='flex gap-2'>
-          <button className='text-xl rounded-full px-4 py-2 bg-green-600 text-white hover:bg-green-700'>Get Started</button>
-          <button className='text-xl border-2 border-green-600 rounded-full px-4 py-2 text-green-600 hover:text-green-700 hover:border-green-700'>Know More</button>
+          <Link to='/signup'><button className='text-xl rounded-full px-4 py-2 bg-green-600 text-white hover:bg-green-700'>Get Started</button></Link>
+          <Link to='/about'><button className='text-xl border-2 border-green-600 rounded-full px-4 py-2 text-green-600 hover:text-green-700 hover:border-green-700'>Know More</button></Link>
         </div>
       </div>
 
@@ -46,17 +46,17 @@ const Home = () => {
       <div className='flex justify-center'>
         <div className='about-us w-3/5'>
           <h3 className='text-4xl font-bold text-green-600 border-b-[6px] border-b-green-600 pb-2'>About Us</h3>
-          <p></p>
+          <p className='p-2'>At Rambo, we believe that every dog deserves care, safety, and love. Our platform connects compassionate volunteers with dedicated NGOs 
+            to create a network of support for stray and injured dogs. By working together, we aim to rescue, rehabilitate, and provide a better future 
+            for animals in need.Our platform empowers volunteers to report cases of dogs in distress, ensuring that help reaches them quickly and efficiently. 
+            NGOs can respond to these reports, track cases, and provide real-time updates — creating a transparent and collaborative rescue process.</p>
         </div>
       </div>
 
       <div className='h-[200px] bg-green-400 flex justify-between items-center px-10 '>
-          <h2 className='text-4xl font-thin text-green-700'>Connect now to make a difference. Join Us.</h2>
+          <h2 className='text-4xl font-thin text-green-700'>Connect Now To Make A Difference. Join Us. One Rescue At A Time.</h2>
           <div className='flex gap-3'>
-            <button className='rounded-3xl px-4 py-2 text-white
-            bg-green-700 hover:bg-green-600 transition-all'>Sign Up as a Volunteer</button>
-            <button className='rounded-3xl px-4 py-2 text-white
-            bg-green-700 hover:bg-green-600 transition-all'>Sign Up as an NGO</button>
+            <Link to='/signup'><button className='rounded-3xl px-4 py-2 text-white bg-green-700 hover:bg-green-600 transition-all'>Sign Up</button></Link>
           </div>
       </div>
     </div>
