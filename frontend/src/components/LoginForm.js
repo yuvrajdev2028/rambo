dae4f4109e23a145cfa382006a0a5643d03915c7
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-04-15 14:02:56 +0530

    added error Alert and password Input

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,54 +1,60 @@
 import React, {useContext, useState} from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGoogle} from '@fortawesome/free-brands-svg-icons'
 import AuthContext from '../contexts/AuthContext';
+import { Link } from 'react-router-dom';
+import PasswordInput from './PasswordInput';
 
 const LoginForm = () => {
   const { login } = useContext(AuthContext);
 
   const [formData,setFormData] = useState({
-      email:"",
-      password:""
-    });
-  
-    const changeHandler = (event)=>{
-      const {name, value} = event.target;
-      setFormData((prev)=>({
-        ...prev,
-        [name]:value
-      }));
-    };
-  
-    const submitHandler = async(event)=>{
-      event.preventDefault();
-      
-      await login(formData.email, formData.password);
+      email:'',
+      password:''
+  });
 
+  const [errorAlert,setErrorAlert] = useState(null);
+  
+  const changeHandler = (event)=>{
+    const {name, value} = event.target;
+    setFormData((prev)=>({
+      ...prev,
+      [name]:value
+    }));
+  };
+  
+  const submitHandler = async(event)=>{
+    event.preventDefault();
+    
+    const result = await login(formData.email, formData.password);
+
+    if(result===401){
+      setErrorAlert('Invalid Email or Password.');
+    }
+    else if(result===500){
+      setErrorAlert('Login failed! Please try again.');
+    }
+    else{
       setFormData({
         email:'',
         password:''
       })
     }
+  }
 
 
   return (
       <form onSubmit={submitHandler} className='flex flex-col'>
         <label htmlFor='email' className='text-left text-sm font-semibold'>EMAIL</label>
         <input type='email' name='email' id='email' value={formData.email} onChange={changeHandler} className='bg-[#EFF0F2] p-2' placeholder='Email' required></input>
+
         <label htmlFor='password' className='text-left text-sm font-semibold mt-2'>PASSWORD</label>
-        <input type='password' name='password' id='password' value={formData.password} onChange={changeHandler} className='bg-[#EFF0F2] p-2' placeholder='Password' required></input>
-        <button className='text-right text-xs mt-2 text-[#838383]'>Forgot Password?</button>
+        <PasswordInput name={'password'} id={'password'} value={formData.password} changeHandler={changeHandler} placeholder={'Password'}/>
+
+        <Link to='/forgot-password'><button type='button' className='text-xs mt-2 text-[#838383]'>Forgot Password?</button></Link>
+        {(errorAlert)?<p className='text-xs text-red-600'>{errorAlert}</p>:<></>}
         <button type='submit' className='bg-green-700 w-36 p-2 text-white mx-auto rounded-3xl my-5 hover:bg-green-600 transition-all'>LogIn</button>
-        <div className='flex items-center justify-between mb-10 mt-5'>
-          <div className='h-[2px] bg-[#EFF0F2] w-[45%]'></div>
-          <div className='text-xs text-[#838383]'>OR</div>
-          <div className='h-[2px] bg-[#EFF0F2] w-[45%]'></div>
-        </div>
-        <div className='flex flex-col items-center'>
-          <h5 className='text-xs text-[#838383]'>Log In Using</h5>
-          <div className='mt-3 text-green-600'>
-          <FontAwesomeIcon icon={faGoogle} />
-          </div>
+        <div className='text-xs text-[#838383] text-center'>
+          <span>Don't have an account? </span>
+          <Link to='/signup'><span className='underline text-green-600'>SignUp</span></Link>
         </div>
       </form>
   )
