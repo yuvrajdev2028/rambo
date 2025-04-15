f137ac2a6d1173f6bf67047c8490e027f073658c
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-04-15 14:10:38 +0530

    fixed height of login component

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,7 +3,7 @@ import LoginForm from '../components/LoginForm'
 
 const Login = () => {
   return (
-    <div className='login-container h-[70%] w-[30%] drop-shadow-md bg-white my-8 mx-auto p-4'>
+    <div className='login-container w-[30%] drop-shadow-md bg-white my-8 mx-auto p-4'>
       <h3 className='font-thin text-2xl mb-10 text-green-600'>LogIn</h3>
       <LoginForm/>
     </div>
