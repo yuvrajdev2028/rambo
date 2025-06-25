1df84f6474cc6f11addf535c752f7e3bcbe2f908
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-06-26 01:23:18 +0530

    updated utilsCOntext to add isPasswordValid function

--- frontend/src/contexts/UtilsContext.js
+++ frontend/src/contexts/UtilsContext.js
@@ -7,8 +7,13 @@ export const UtilsProvider = ({children})=>{
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         return emailRegex.test(email);
     }
+    const isPasswordValid = (password)=>{
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%&])[A-Za-z\d@#$%&]{8,}$/;
+        return passwordRegex.test(password);
+
+    }
     return(
-        <UtilsContext.Provider value={{isEmailValid}}>
+        <UtilsContext.Provider value={{isEmailValid, isPasswordValid}}>
             {children}
         </UtilsContext.Provider>
     )
