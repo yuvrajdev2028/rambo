f74ca0bd12d1fc14a8cae20d10ec795abe1cd34c
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-07-07 12:20:48 +0530

    Refactored code

--- frontend/src/components/VolunteerDisplay.js
+++ frontend/src/components/VolunteerDisplay.js
@@ -2,25 +2,12 @@ import React, { useContext, useState } from 'react'
 import AuthContext from '../contexts/AuthContext'
 import MetricCard from './MetricCard';
 import MyReportsCard from './MyReportsCard';
+import SubmitReport from './SubmitReport';
+
+const apiUrl = process.env.REACT_APP_BASE_URL;
 
 const VolunteerDisplay = ({tab,setTab}) => {
   const { user } = useContext(AuthContext);
-
-  const [formData,setFormData] = useState({
-    title:"",
-    description:"",
-    location:"",
-    imageUrl:"",
-    ngo:"",
-  });
-
-  const changeHandler = (event)=>{
-    const {name, value} = event.target;
-    setFormData((prev)=>({
-      ...prev,
-      [name]:value
-    }));
-  };
 
   if(tab==='Profile'){
     return (
@@ -41,21 +28,9 @@ const VolunteerDisplay = ({tab,setTab}) => {
   }
   else if(tab==='Submit Report'){
     return(
-      <div className='flex-grow'>
-        <form className='flex flex-col h-[70%] w-[30%] drop-shadow-md bg-white my-8 mx-auto p-4'>
-        <label htmlFor='title' className='text-left text-sm font-semibold'>Title</label>
-        <input type='text' name='title' id='title' value={formData.title} onChange={changeHandler} className='bg-[#EFF0F2] p-2' placeholder='Title' required />
-        <label htmlFor='description' className='text-left text-sm font-semibold'>Description</label>
-        <textarea name='description' id='description' value={formData.description} onChange={changeHandler} className='bg-[#EFF0F2] p-2' placeholder='Description' required />
-        <label htmlFor='location' className='text-left text-sm font-semibold'>Location</label>
-        <input type='text' name='location' id='location' value={formData.location} onChange={changeHandler} className='bg-[#EFF0F2] p-2' placeholder='Location' required />
-        <label htmlFor='image' className='text-left text-sm font-semibold'>Image</label>
-        <input type='text' name='image' id='image' value={formData.imageUrl} onChange={changeHandler} className='bg-[#EFF0F2] p-2' placeholder='Image' required />
-        <label htmlFor='ngo' className='text-left text-sm font-semibold'>NGO</label>
-        <input type='text' name='ngo' id='ngo' value={formData.ngo} onChange={changeHandler} className='bg-[#EFF0F2] p-2' placeholder='NGO' required />
-        <button type='submit' className='bg-green-600 w-36 p-2 text-white mx-auto rounded-md my-5 hover:bg-green-500 transition-all'>Submit Report</button>
-        </form>
-      </div>
+      <>
+        <SubmitReport />
+      </>
     )
   }
   else if(tab==='My Reports'){
