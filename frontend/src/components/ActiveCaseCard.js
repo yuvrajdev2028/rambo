4711eddb8d1e286899ab070bfbcc35095aa9ef8a
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-07-22 23:48:11 +0530

    fixed active case card

--- frontend/src/components/ActiveCaseCard.js
+++ frontend/src/components/ActiveCaseCard.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SubmitResponseModal from './SubmitResponseModal';
 
-const ActiveCaseCard = () => {
+const ActiveCaseCard = ({title,date,description,image,reportId,ngoId}) => {
+  const [showResponseModal,setShowResponseModal] = useState(false);
+
   return (
-    <div className='border rounded-md my-4 w-1/2 mx-auto p-4'>
+    <div className='bg-slate-900 rounded-md my-4 w-1/2 mx-auto p-4'>
         <div className='flex justify-between mb-2'>
-            <span className='font-bold text-green-600'>Sample Title</span>
-            <span>17/01/2025</span>
+            <span className='font-bold text-green-600'>{title}</span>
+            <span>{date}</span>
         </div>
-        <p className='mb-2'>Sample Description which is just long for the sake of filling space. Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+        <img src={image} alt='Submitted to you for this report' className='w-1/4 mx-auto h-auto'/>
+        <p className='mb-2'>{description}</p>
         <div>
-            <button className='border border-green-600 rounded-md py-2 px-4 hover:bg-green-600 hover:text-white'>Resolve</button>
+            <button className='border border-green-600 rounded-md py-2 px-4 hover:bg-green-600 hover:text-white' onClick={()=>{setShowResponseModal(true)}}>Resolve</button>
         </div>
+        {showResponseModal && <SubmitResponseModal reportId={reportId} ngoId={ngoId} setShowResponseModal={setShowResponseModal}/>}
     </div>
   )
 }
