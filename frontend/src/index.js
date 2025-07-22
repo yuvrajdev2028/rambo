12e9214d37cc53b38e40ff42aaeddbaee1c8639f
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-07-22 23:44:42 +0530

    Added query client

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { UtilsProvider } from './contexts/UtilsContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
 root.render(
   <BrowserRouter>
     <AuthProvider>
       <UtilsProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </UtilsProvider>
     </AuthProvider>
   </BrowserRouter>
