e75bba84a48eb770ba8019b16f2b9e0cdc49c108
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-07-22 23:41:57 +0530

    added a route to update status and paired it with update Status controller

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {auth,isNgo,isVolunteer} = require('../middlewares/authMiddleware');
-const {createReport,getAllReports} = require('../controllers/reportsControllers');
+const {createReport,getAllReports,updateStatus} = require('../controllers/reportsControllers');
 
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
@@ -10,5 +10,6 @@ const router = express.Router();
 router.post('/createReport',auth,isVolunteer,upload.single('image'),createReport);
 router.get('/ngo/getAllReports',auth,isNgo,getAllReports);
 router.get('/volunteer/getAllReports',auth,isVolunteer,getAllReports);
+router.put('/ngo/updateStatus',auth,isNgo,updateStatus);
 
 module.exports = router;
